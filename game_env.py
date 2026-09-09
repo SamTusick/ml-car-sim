bd865db8e60e236cce5307cc209c9e7b7f90d1dc
SamTusick <stusick@outlook.com>
2026-09-08 22:00:06 -0400

    Set boundry limits. If obj goes past limits it will reset to middle

--- game_env.py
+++ game_env.py
@@ -1,14 +1,21 @@
-# Example file showing a circle moving on screen
 import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen_middle_x = SCREEN_WIDTH / 2
+screen_middle_y = SCREEN_HEIGHT / 2
+
 clock = pygame.time.Clock()
 running = True
-dt = 0
+dt = 0      
 
-rect1 = pygame.Rect(screen.get_width() / 2,screen.get_height() / 2 ,10,20)
+# Object Setup
+rect1 = pygame.Rect(screen_middle_x,screen_middle_y,10,20)
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -19,22 +26,10 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("red")
 
-    pygame.draw.rect(screen, "white", rect1)
-
     # Read keyboard input 
     keys = pygame.key.get_pressed()
 
-    # WASD
-    if keys[pygame.K_w]:
-        rect1.y -= 300 * dt
-    if keys[pygame.K_s]:
-        rect1.y += 300 * dt
-    if keys[pygame.K_a]:
-        rect1.x -= 300 * dt
-    if keys[pygame.K_d]:
-        rect1.x += 300 * dt
-
-    # Arrow Keys    
+    # Arrow Keys Movement   
     if keys[pygame.K_UP]:
         rect1.y -= 300 * dt
     if keys[pygame.K_DOWN]:
@@ -44,12 +39,16 @@ while running:
     if keys[pygame.K_RIGHT]:
         rect1.x += 300 * dt
 
+    if rect1.y > SCREEN_HEIGHT or rect1.x > SCREEN_WIDTH or rect1.y < 0 or rect1.x < 0:
+            rect1.x, rect1.y = screen_middle_x, screen_middle_y
+
+    pygame.draw.rect(screen, "white", rect1)
+
+    pygame.display.update()
+
     # If 'q' gets pressed close game
     if keys[pygame.K_q]:
         running = False
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
 
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
